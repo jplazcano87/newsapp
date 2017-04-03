411d135fa34ceeb9854e97cca523e40dafbcf7fd
Juan Pablo Lazcano <juanpablo.lazcano@beetrack.com>
2017-04-02 22:30:21 -0300

    added snackbar

--- app/src/main/java/news/spaceghost/com/newsapp/MainActivity.java
+++ app/src/main/java/news/spaceghost/com/newsapp/MainActivity.java
@@ -1,9 +1,14 @@
 package news.spaceghost.com.newsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import news.spaceghost.com.newsapp.model.GetArticlesResponse;
@@ -15,13 +20,14 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
   private RecyclerView newsRecyclerView;
+  private CoordinatorLayout mCoordinatorLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-
+    mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
     newsRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerview);
     newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     call.enqueue(new Callback<GetArticlesResponse>() {
       @Override
       public void onResponse(Call<GetArticlesResponse> call, Response<GetArticlesResponse> response) {
+        showNewsApiSnack();
         GetArticlesResponse getArticlesResponse = response.body();
         NewsStore.setNewsArticles(getArticlesResponse.getArticles());
         Toast.makeText(MainActivity.this, "response received", Toast.LENGTH_SHORT).show();
@@ -44,5 +51,19 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "response received", Toast.LENGTH_SHORT).show();
       }
     });
+  }
+
+  private void showNewsApiSnack(){
+    Snackbar.make(mCoordinatorLayout, "Powered by NewsApi.org", Snackbar.LENGTH_LONG).setAction
+        ("Visit", new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            loadNewsWebsite();
+          }
+        }).show();
+  }
+
+  private void loadNewsWebsite(){
+    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://newsapi.org")));
   }
 }
